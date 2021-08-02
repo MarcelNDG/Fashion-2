22257d5b5ea852100e79dbfa85d013059915c9fc
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-08-02 16:19:22 -0400

    Absolute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
+
+import Model from './img/model.png'
+
 import TopNav from './components/TopNav'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
@@ -8,7 +13,15 @@ function App() {
      <header>
        
        <TopNav />
+             
      </header>
+     <main>
+     <h1>FASHION <br></br><span>FASCINATION</span></h1>
+      <img className='modelPic' src={Model} alt="Model" />
+     </main>
+     <footer>
+       <Footer />
+     </footer>
     </div>
   );
 }
---
+++ src/components/LogoPlaceholder.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LogoPLaceholder = () => {
+    return(
+        <div className='logo'>
+            <h1>FASHION <br></br><span>FASCINATION</span></h1>
+        </div>
+    )
+}
+
+
+export default LogoPLaceholder

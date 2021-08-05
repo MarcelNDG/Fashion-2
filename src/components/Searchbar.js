f4c783ae80cbda7328956c206355e471f3d25d00
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-08-05 11:32:05 -0400

    PIc fixed, search bar size

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Searchbar = () => {
     return(
-        <div >
+        
             <input className='searchbar' type="text" placeholder="Search.."></input>
-        </div>
+        
     )
 }
 

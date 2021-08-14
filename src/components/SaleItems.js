da0d66d277d71375780dce07b8c1addc344135bb
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-08-14 11:00:20 -0400

    css higiene

---
+++ src/components/SaleItems.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SaleItems = () => {
+    return (
+        <>
+            <div className='saleContainer'>
+                
+            </div>
+        </>
+    )
+}
+
+export default SaleItems

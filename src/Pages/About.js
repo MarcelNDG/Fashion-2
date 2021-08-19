979cc757446c2791b2c5179cfef247bd70bd0fe6
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-08-19 11:21:19 -0400

    Signature added not styled
    need to add image support for moto g4

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,6 +5,9 @@ import SphereBig from '../components/SphereBig'
 
 import Model2 from '../img/model2.png'
 
+import { ReactComponent as Signature } from '../svg/signature.svg'
+
+
 
 const About = () => {
     return (
@@ -13,8 +16,9 @@ const About = () => {
                 <TopNav />
             </header>
             <main className='aboutMain'>
-                <h2>About Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget egestas diam. Nam tincidunt sit amet nunc in hendrerit. Praesent dapibus ligula vitae nisi posuere sodales. Morbi tempor sodales bibendum. Quisque elementum felis nec fringilla malesuada. Suspendisse potenti. Pellentesque augue dolor, finibus sed laoreet id, pretium a velit. Nam commodo sagittis magna a faucibus. Vivamus pharetra pulvinar ligula id viverra. Praesent aliquam est ex, dignissim porttitor ipsum cursus et. Maecenas in maximus mi, sed posuere nulla. Aenean sit amet dictum velit. Proin vestibulum ante sed odio tempus, sed euismod leo sagittis. Vivamus porttitor hendrerit dolor et molestie. Quisque sagittis, justo sit amet consectetur ornare, nunc quam laoreet quam, vitae ornare leo libero non lorem.</p>
+                <h2>About Us...</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget egestas diam. Nam tincidunt sit amet nunc in hendrerit. Praesent dapibus ligula vitae nisi posuere sodales. Morbi tempor sodales bibendum. Quisque elementum felis nec fringilla malesuada. Suspendisse potenti. Pellentesque augue dolor, finibus sed laoreet id, pretium a velit. Nam commodo sagittis magna a faucibus. Vivamus pharetra pulvinar ligula id viverra. Praesent aliquam est ex, dignissim porttitor ipsum cursus et. Maecenas in maximus mi, sed posuere nulla. Aenean sit amet dictum velit. Proin vestibulum ante sed odio tempus, sed euismod leo sagittis. Vivamus porttitor hendrerit dolor et molestie. </p>
+                <Signature className='signature' />           
             </main>
             <div className="image-container">
                 <SphereBig />

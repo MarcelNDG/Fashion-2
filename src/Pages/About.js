6d3c846973225166642c0072146a506331d19a42
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-08-19 16:51:11 -0400

    about done 1.0

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -16,9 +16,11 @@ const About = () => {
                 <TopNav />
             </header>
             <main className='aboutMain'>
-                <h2>About Us...</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget egestas diam. Nam tincidunt sit amet nunc in hendrerit. Praesent dapibus ligula vitae nisi posuere sodales. Morbi tempor sodales bibendum. Quisque elementum felis nec fringilla malesuada. Suspendisse potenti. Pellentesque augue dolor, finibus sed laoreet id, pretium a velit. Nam commodo sagittis magna a faucibus. Vivamus pharetra pulvinar ligula id viverra. Praesent aliquam est ex, dignissim porttitor ipsum cursus et. Maecenas in maximus mi, sed posuere nulla. Aenean sit amet dictum velit. Proin vestibulum ante sed odio tempus, sed euismod leo sagittis. Vivamus porttitor hendrerit dolor et molestie. </p>
-                <Signature className='signature' />           
+                <div className='aboutContainer'>
+                    <h2>About Us...</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget egestas diam. Nam tincidunt sit amet nunc in hendrerit. Praesent dapibus ligula vitae nisi posuere sodales. Morbi tempor sodales bibendum. Quisque elementum felis nec fringilla malesuada. Suspendisse potenti. Pellentesque augue dolor, finibus sed laoreet id, pretium a velit. Nam commodo sagittis magna a faucibus. Vivamus pharetra pulvinar ligula id viverra. Praesent aliquam est ex, dignissim porttitor ipsum cursus et. Maecenas in maximus mi, sed posuere nulla. Aenean sit amet dictum velit. Proin vestibulum ante sed odio tempus, sed euismod leo sagittis. Vivamus porttitor hendrerit dolor et molestie. </p>
+                    <Signature className='signature' />
+                </div>                           
             </main>
             <div className="image-container">
                 <SphereBig />

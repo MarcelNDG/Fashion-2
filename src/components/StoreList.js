1f4b1f91d56bf8bd9f59ad0cddc4f634f1afc1c7
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-08 15:43:49 -0300

    Store mobile done

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -5,6 +5,7 @@ import Store from './Store'
 const StoreList = () => {
     return (
         <div className='storeList'>
+            <h2>Check our stores...</h2>
             <div className='storeContainer'>
                 <Store location={'Mordor Gates'} adress={'Avenida siempreviva #1234'} region={'Concepción'} schedule={'00:07 AM - 00:08 PM'}/>
                 <Store location={'Mordor Gates'} adress={'Avenida siempreviva #1234'} region={'Concepción'} schedule={'00:07 AM - 00:08 PM'}/>

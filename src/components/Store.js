57f332b0393ea277d6c0f27b34be581dbc364837
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-08 16:00:02 -0300

    store class fix

--- src/components/Store.js
+++ src/components/Store.js
@@ -5,13 +5,16 @@ const Store = ({location, adress, region, schedule}) => {
         <div className='store'>
             <div className='storeData'>
                 <h3>{location}</h3>
-                <div className='storeAdressContainer'>
-                    <h4>{adress}</h4>
-                    <h4>{region}</h4>
-                    <h4>{schedule}</h4>
-                    <button className='storeMapBtn'>Check in Google Maps</button>
+                <div className='storeContainer'>
+                    <div className='storeAdressContainer'>
+                        <h4>{adress}</h4>
+                        <h4>{region}</h4>
+                        <h4>{schedule}</h4>
+                        <button className='storeMapBtn'>Check in Google Maps</button>
+                    </div>
+                    <div className='storeImgPLaceholder'></div>
                 </div>
-                <div className='storeImgPLaceholder'></div>
+                
                 
             </div>
         </div>
--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -6,7 +6,7 @@ const StoreList = () => {
     return (
         <div className='storeList'>
             <h2>Check our stores...</h2>
-            <div className='storeContainer'>
+            <div className='storeListContainer'>
                 <Store location={'Mordor Gates'} adress={'Avenida siempreviva #1234'} region={'Concepción'} schedule={'00:07 AM - 00:08 PM'}/>
                 <Store location={'Mordor Gates'} adress={'Avenida siempreviva #1234'} region={'Concepción'} schedule={'00:07 AM - 00:08 PM'}/>
                 <Store location={'Mordor Gates'} adress={'Avenida siempreviva #1234'} region={'Concepción'} schedule={'00:07 AM - 00:08 PM'}/>

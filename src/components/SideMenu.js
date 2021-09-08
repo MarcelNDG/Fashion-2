81d16957a9d7af2965a37bea7995f6d46c264f30
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-08 10:50:12 -0300

    Sales buttons mobile

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -7,57 +7,53 @@ import { ReactComponent as ProfileBtn } from '../icon/usermenu.svg'
 import { ReactComponent as Kart } from '../icon/shopping-cart.svg'
 import { ReactComponent as ConfigBtn } from '../icon/setting-lines.svg'
 
-
-
 const SideMenu = () => {
     return (
         <>
         <div className='sideMenu noMenu' id='sideMenu'> 
-        <div className='sideProfile'>
-            <div className='profileIconMobile'>
-                <button className='userButtonMobile'>
-                    <ProfileBtn />
-                </button>
-                    <h3>Profile
-            </h3>
+            <div className='sideProfile'>
+                <div className='profileIconMobile'>
+                    <button className='userButtonMobile'>
+                        <ProfileBtn />
+                    </button>
+                        <h3>Profile</h3>
+                </div>
+                <div className='profileIconMobile'>
+                    <button className='userButtonMobile'>
+                        <Kart />
+                    </button>
+                        <h3>Check
+                            <br></br>
+                            <span>Kart</span>
+                        </h3>
+                </div>
+                <div className='profileIconMobile'>
+                    <button className='userButtonMobile'>
+                        <ConfigBtn />
+                    </button>
+                        <h3>Profile</h3>
+                </div>
             </div>
-            <div className='profileIconMobile'>
-                <button className='userButtonMobile'>
-                    <Kart />
-                </button>
-                    <h3>Check
-                        <br></br>
-                        <span>Kart</span>
-                    </h3>
+
+            <div className='sideSales'>
+                <button>Sale Button</button>
+                <button>Sale Button</button>
+                <button>Sale Button</button>
             </div>
-            <div className='profileIconMobile'>
-                <button className='userButtonMobile'>
-                    <ConfigBtn />
-                </button>
-                    <h3>Profile
-            </h3>
-            </div>
-            
-            <button> </button>
-            <button> </button>
-            
-        </div>
-             
-            <div className='sideContent'>
                 
-                    <button >
-                        <Link to='/About' className='sideBtn'>About</Link >
-                        </button> 
-                    <button>
-                        <Link to='/Sale' className='sideBtn'>Sale</Link>
-                        </button> 
-                    <button>
-                        <Link to='/Stores' className='sideBtn'>Stores</Link>
-                        </button> 
-                    <button>
-                        <Link to='/Contact' className='sideBtn'>Contact</Link>
-                        </button> 
-                
+            <div className='sideContent'>                
+                <button >
+                    <Link to='/About' className='sideBtn'>About</Link >
+                    </button> 
+                <button>
+                    <Link to='/Sale' className='sideBtn'>Sale</Link>
+                    </button> 
+                <button>
+                    <Link to='/Stores' className='sideBtn'>Stores</Link>
+                    </button> 
+                <button>
+                    <Link to='/Contact' className='sideBtn'>Contact</Link>
+                </button> 
             </div>
         </div>
         </>

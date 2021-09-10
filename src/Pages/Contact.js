cf3af9bb6710eca75e62faa80c923dc0c413a7a4
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-10 17:05:16 -0300

    contact start

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,12 +4,20 @@ import TopNav from '../components/TopNav'
 import Footer from '../components/Footer'
 
 const Contact = () => {
+
     return (
         <>
         <header>
             <TopNav />
         </header>
-        <main></main>
+        <main className='contactMain'>
+            <div className='contactContainer'>
+                <h2>Contact us...</h2>
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin egestas fermentum efficitur. Vivamus volutpat risus augue, vitae tristique diam interdum ut. Nam eget libero commodo risus tristique bibendum ut in turpis. Nulla lacinia condimentum mauris ac dictum. Cras malesuada nibh finibus orci ultrices scelerisque.</p>
+            </div>
+        </main>
+
         <Footer />
         </>
     )

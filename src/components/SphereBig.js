6f639a0576ce4b70065249d0b2c140c39220dc28
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-15 16:47:27 -0300

    SphereBig fade in done

--- src/components/SphereBig.js
+++ src/components/SphereBig.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
+
 
 const SphereBig = () => {
+
+    useEffect(() => {
+        const sphereBig = document.getElementById('sphereBig')
+        
+        sphereBig.style.opacity = '1'
+        sphereBig.style.transition = '1.8s'
+        return () => {
+            
+        }
+    }, [])
     return(
         <>
-            <div className='sphereBig'></div>
+            <div className='sphereBig' id='sphereBig'></div>
         </>
     )
 }

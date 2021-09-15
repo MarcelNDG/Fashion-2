587f9703b494977927c452ac839974ef7855d09c
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2021-09-15 10:29:20 -0300

    Profile info start

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { ReactComponent as ProfileBtn } from '../icon/usermenu.svg'
+
 import TopNav from '../components/TopNav'
 import Footer from '../components/Footer'
 
@@ -8,7 +10,32 @@ const Profile = () => {
         <>
         <header>            
             <TopNav />    
-        </header>    
+        </header>
+        <main>
+            <div className='profileWrap'>
+                <div className='avatarContainer'>
+                    
+                    <ProfileBtn className='profileAvatar' />
+                    <div className='avatarInfo'>
+                        <h3>Username</h3>
+                        <h4>user_email@gmail.com</h4>
+                        <a href='./'>Change account e-mail</a>
+                    </div>
+                </div>
+                <div className='profileformContainer'>
+                    
+                        <div className='formItem'>
+                            <h3>Adress: Avenida siempreviva #1234</h3>
+                            <button>Change adress</button>
+                            <input type='text'></input>
+                        </div>
+                        
+                    
+                </div>
+
+            </div>
+            
+        </main>  
             
         <Footer />
         </>
